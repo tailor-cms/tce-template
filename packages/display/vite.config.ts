111186ee5e322f7aba82f790766cd86eacffd6e5
underscope <underscope@gmail.com>
2023-09-05 10:28:41 +0200

    Tweak component build
    - Externalize vue

--- packages/display/vite.config.ts
+++ packages/display/vite.config.ts
@@ -5,15 +5,27 @@ import { resolve } from 'node:path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [vue()],
   build: {
+    minify: false,
+    cssCodeSplit: false,
     lib: {
       entry: resolve(__dirname, './src/index.ts'),
-      name: 'Display',
+      name: 'TceDisplay',
       fileName: 'index',
       formats: ['es', 'cjs'],
     },
-    minify: false,
-    cssCodeSplit: false,
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
   },
-  plugins: [vue()],
 });
