73811b94610ad0bc2dc350dfb1de815cdf137eb9
underscope <underscope@gmail.com>
2023-09-11 12:04:01 +0200

    Update browser

--- test/playwright.config.ts
+++ test/playwright.config.ts
@@ -24,8 +24,10 @@ export default defineConfig({
   },
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: 'firefox',
+      use: {
+        ...devices['Desktop Firefox'],
+      },
     },
   ]
 });
