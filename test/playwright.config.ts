1b021a7e73970782fe148b715292beabcecd74e3
underscope <underscope@gmail.com>
2023-09-11 11:46:34 +0200

    Disable parallel run

--- test/playwright.config.ts
+++ test/playwright.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   testDir: '.',
   outputDir: './out',
   timeout: 60000,
-  fullyParallel: true,
+  fullyParallel: false,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
