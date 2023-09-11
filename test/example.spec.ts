b732785fa1f6d44a26a6b084a1a3bf238ec53d3c
underscope <underscope@gmail.com>
2023-09-11 12:47:08 +0200

    Temp disable Edit component

--- test/example.spec.ts
+++ test/example.spec.ts
@@ -11,16 +11,16 @@ test('Renders Display component', async ({ page }) => {
   await expect(displayFrame.getByText(DISPLAY_COPY)).toBeVisible();
 });
 
-test('Renders Edit component', async ({ page }) => {
-  const editFrame = page.frameLocator('#editPanel')
-  await expect(editFrame.getByText('Edit preview')).toBeVisible();
-  const EDIT_COPY = 'Edit version of the content element';
-  await expect(editFrame.getByText(EDIT_COPY)).toBeVisible();
-  const TOP_TOOLBAR_COPY = 'Edit element top toolbar';
-  await expect(editFrame.getByText(TOP_TOOLBAR_COPY)).toBeVisible();
-  const SIDE_TOOLBAR_COPY = 'Edit element side toolbar';
-  await expect(editFrame.getByText(SIDE_TOOLBAR_COPY)).toBeVisible();
-});
+// test('Renders Edit component', async ({ page }) => {
+//   const editFrame = page.frameLocator('#editPanel')
+//   await expect(editFrame.getByText('Edit preview')).toBeVisible();
+//   const EDIT_COPY = 'Edit version of the content element';
+//   await expect(editFrame.getByText(EDIT_COPY)).toBeVisible();
+//   const TOP_TOOLBAR_COPY = 'Edit element top toolbar';
+//   await expect(editFrame.getByText(TOP_TOOLBAR_COPY)).toBeVisible();
+//   const SIDE_TOOLBAR_COPY = 'Edit element side toolbar';
+//   await expect(editFrame.getByText(SIDE_TOOLBAR_COPY)).toBeVisible();
+// });
 
 test('Renders server state panel', async ({ page }) => {
   const properties = ['uid', 'type', 'meta', 'data', 'contentId'];
