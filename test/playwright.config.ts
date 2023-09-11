2cf04477909ccabf486db1b1c9a825ab4af6c74f
underscope <underscope@gmail.com>
2023-09-11 11:04:45 +0200

    Increase timeout

--- test/playwright.config.ts
+++ test/playwright.config.ts
@@ -12,6 +12,7 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: '.',
   outputDir: './out',
+  timeout: 60000,
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
