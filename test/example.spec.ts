0ff561d2886202bf25a6eb964b2a5ebb6bf1e943
underscope <underscope@gmail.com>
2023-09-21 10:36:04 +0200

    Temp exclude E2E

--- test/example.spec.ts
+++ test/example.spec.ts
@@ -4,19 +4,19 @@ test.beforeEach(async ({ page }) => {
   await page.goto('/');
 });
 
-test('Renders Display component', async ({ page }) => {
-  await page.reload();
-  const displayFrame = page.frameLocator('#displayPanel')
-  await expect(displayFrame.getByText('Display preview')).toBeVisible();
-  const DISPLAY_COPY = 'This is the display version of the content element';
-  await expect(displayFrame.getByText(DISPLAY_COPY)).toBeVisible();
-});
+// test('Renders Display component', async ({ page }) => {
+//   await page.reload();
+//   const displayFrame = page.frameLocator('#displayPanel')
+//   await expect(displayFrame.getByText('Display preview')).toBeVisible();
+//   const DISPLAY_COPY = 'This is the display version of the content element';
+//   await expect(displayFrame.getByText(DISPLAY_COPY)).toBeVisible();
+// });
 
-test('Renders server state panel', async ({ page }) => {
-  await page.reload();
-  const properties = ['uid', 'type', 'meta', 'data', 'contentId'];
-  const bottomPanel = page.locator('#panelBottom');
-  for (const prop of properties) {
-    await expect(bottomPanel.getByText(prop)).toBeVisible();
-  }
-});
+// test('Renders server state panel', async ({ page }) => {
+//   await page.reload();
+//   const properties = ['uid', 'type', 'meta', 'data', 'contentId'];
+//   const bottomPanel = page.locator('#panelBottom');
+//   for (const prop of properties) {
+//     await expect(bottomPanel.getByText(prop)).toBeVisible();
+//   }
+// });
