cccd77551e11e58f349e7da22da5c20d32adedfe
underscope <underscope@gmail.com>
2023-09-22 18:57:34 +0200

    Add Edit component test

--- test/example.spec.ts
+++ test/example.spec.ts
@@ -4,8 +4,18 @@ test.beforeEach(async ({ page }) => {
   await page.goto('/');
 });
 
+test('Renders Edit component', async ({ page }) => {
+  const editFrame = page.frameLocator('#editPanel')
+  await expect(editFrame.getByText('Edit preview')).toBeVisible();
+  const EDIT_COPY = 'Edit version of the content element';
+  await expect(editFrame.getByText(EDIT_COPY)).toBeVisible();
+  const TOP_TOOLBAR_COPY = 'Edit element top toolbar';
+  await expect(editFrame.getByText(TOP_TOOLBAR_COPY)).toBeVisible();
+  const SIDE_TOOLBAR_COPY = 'Edit element side toolbar';
+  await expect(editFrame.getByText(SIDE_TOOLBAR_COPY)).toBeVisible();
+});
+
 test('Renders Display component', async ({ page }) => {
-  await page.reload();
   const displayFrame = page.frameLocator('#displayPanel')
   await expect(displayFrame.getByText('Display preview')).toBeVisible();
   const DISPLAY_COPY = 'This is the display version of the content element';
@@ -13,7 +23,6 @@ test('Renders Display component', async ({ page }) => {
 });
 
 test('Renders server state panel', async ({ page }) => {
-  await page.reload();
   const properties = ['uid', 'type', 'meta', 'data', 'contentId'];
   const bottomPanel = page.locator('#panelBottom');
   for (const prop of properties) {
