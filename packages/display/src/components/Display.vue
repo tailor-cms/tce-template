69e7a37ea54d13a9af4afe3edd677287b8c0e6c0
underscope <underscope@gmail.com>
2023-11-09 18:16:05 +0100

    Add user state mgm example

--- packages/display/src/components/Display.vue
+++ packages/display/src/components/Display.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="tce-root">
     <p>This is the Display version of the content element {{ id }}</p>
+    <v-btn class="my-6" @click="submit">Update user state</v-btn>
+    <div>User state: {{ userState }}</div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ElementData } from 'tce-manifest';
 
-defineProps<{ id: number; data: ElementData }>();
+const props = defineProps<{ id: number; data: ElementData; userState: any }>();
+const emit = defineEmits(['interaction']);
+
+const submit = () => emit('interaction', { id: props.id });
 </script>
 
 <style scoped>
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,6 +1,11 @@
 import type { HookServices, ServerRuntime } from '@tailor-cms/cek-common';
-import { initState, type } from 'tce-manifest';
+import { initState, mocks, type } from 'tce-manifest';
 import type { Element } from 'tce-manifest';
+
+// Detect if hooks are running in CEK (used for mocking end-system runtime)
+const IS_CEK = process.env.CEK_RUNTIME;
+// Don't use in production, use only when IS_CEK=true
+const USER_STATE: any = {};
 
 /* eslint-disable @typescript-eslint/no-unused-vars */
 export function beforeSave(element: Element, services: HookServices) {
@@ -31,12 +36,38 @@ export function afterRetrieve(
   return element;
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function beforeDisplay(element: Element, context: any) {
+  console.log('beforeDisplay hook');
+  console.log('beforeDisplay context', context);
+  return USER_STATE;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function onUserInteraction(
+  element: Element,
+  context: any,
+  payload: any,
+): any {
+  console.log('onUserInteraction', context, payload);
+  // Simulate user state update within CEK
+  if (IS_CEK) {
+    USER_STATE.interactionTimestamp = new Date().getTime();
+    Object.assign(USER_STATE, payload);
+  }
+  // Can have arbitrary return value (interpreted by target system)
+  // FE is updated if updateDisplayState is true
+  return { updateDisplayState: true };
+}
+
 export const hookMap = new Map(
   Object.entries({
     beforeSave,
     afterSave,
     afterLoaded,
     afterRetrieve,
+    onUserInteraction,
+    beforeDisplay,
   }),
 );
 
@@ -48,6 +79,9 @@ export default {
   afterSave,
   afterLoaded,
   afterRetrieve,
+  onUserInteraction,
+  beforeDisplay,
+  mocks,
 };
 
-export { type, initState };
+export { type, initState, mocks };
