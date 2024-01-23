33f0fc227d67164635ab4699e3cec9912345a672
underscope <underscope@gmail.com>
2024-01-23 09:38:19 +0100

    Add counter example to tce-template

--- packages/edit/src/components/Edit.vue
+++ packages/edit/src/components/Edit.vue
@@ -1,23 +1,41 @@
 <template>
   <div class="tce-container">
-    <span>This is Edit version of the content element {{ element?.id }}</span>
+    <div>This is Edit version of the content element id: {{ element?.id }}</div>
+    <div class="mt-6 mb-2">
+      Counter:
+      <span class="font-weight-bold">{{ element.data.count }}</span>
+    </div>
+    <button @click="increment">Increment</button>
   </div>
 </template>
 
 <script setup lang="ts">
 import { Element } from 'tce-manifest';
 
-defineProps<{ element: Element; isFocused: boolean }>();
-defineEmits(['save']);
+const emit = defineEmits(['save']);
+const props = defineProps<{ element: Element; isFocused: boolean }>();
+
+const increment = () => {
+  const { data } = props.element;
+  const count = data.count + 1;
+  emit('save', { ...data, count });
+};
 </script>
 
 <style scoped>
 .tce-container {
   background-color: transparent;
   margin-top: 1rem;
-  padding: 1rem;
+  padding: 1.5rem;
   border: 2px dashed #888;
   font-family: Arial, Helvetica, sans-serif;
   font-size: 1rem;
+}
+
+button {
+  margin: 1rem 0 0 0;
+  padding: 0.25rem 1rem;
+  background-color: #eee;
+  border: 1px solid #444;
 }
 </style>
