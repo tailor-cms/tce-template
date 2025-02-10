20418e1c0d02241434f2ab80a939f2c917a7efcf
rkusan00 <renato.kusanovic@gmail.com>
2025-02-10 23:16:17 +0100

    Update test

--- test/example.spec.ts
+++ test/example.spec.ts
@@ -6,19 +6,19 @@ test.beforeEach(async ({ page }) => {
 
 test('Renders Edit component', async ({ page }) => {
   const editFrame = page.frameLocator('#editPanel')
-  await expect(editFrame.getByText('Edit preview')).toBeVisible();
+  await expect(editFrame.getByText('Authoring component')).toBeVisible();
   const EDIT_COPY = 'Edit version of the content element';
   await expect(editFrame.getByText(EDIT_COPY)).toBeVisible();
-  const TOP_TOOLBAR_COPY = 'Edit element top toolbar';
+  const TOP_TOOLBAR_COPY = 'Top toolbar';
   await expect(editFrame.getByText(TOP_TOOLBAR_COPY)).toBeVisible();
-  const SIDE_TOOLBAR_COPY = 'Edit element side toolbar';
+  const SIDE_TOOLBAR_COPY = 'Side toolbar';
   await expect(editFrame.getByText(SIDE_TOOLBAR_COPY)).toBeVisible();
 });
 
 test('Renders Display component', async ({ page }) => {
   const displayFrame = page.frameLocator('#displayPanel')
-  await expect(displayFrame.getByText('Display preview')).toBeVisible();
-  const DISPLAY_COPY = 'This is the display version of the content element';
+  await expect(displayFrame.getByText('End-user component')).toBeVisible();
+  const DISPLAY_COPY = 'Display version of the content element';
   await expect(displayFrame.getByText(DISPLAY_COPY)).toBeVisible();
 });
 
